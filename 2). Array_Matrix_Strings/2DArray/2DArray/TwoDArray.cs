23abbd8892c31ab11e671b4ca7a9c11ad6e322e3
ankitmehrotrachemistry <ankitmehrotrachemistry@gmail.com>
2022-08-05 19:02:59 +0530

    ArrayProgram

---
+++ 2). Array_Matrix_Strings/2DArray/2DArray/TwoDArray.cs
@@ -0,0 +1,29 @@
+﻿// C# Program to print 2D Matrix
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2DArray
+{
+    class TwoDArray
+    {
+        static void Main(string[] args)
+        {
+            int[,] a = new int[5, 2] { { 0, 0 }, { 1, 2 }, { 2, 4 }, { 3, 6 }, { 4, 8 } };
+            int i, j;
+
+            for (i = 0; i < 5; i++)
+            {
+                for (j = 0; j < 2; j++)
+                {
+                    Console.WriteLine("a[{0},{1}] = {2}", i, j, a[i, j]);
+                    // {0} is the placeholder for the first argument after the format string, {1} is the second, and so on.
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
