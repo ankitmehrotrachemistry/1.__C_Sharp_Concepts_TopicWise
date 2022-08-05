db61ea095cb0f4b3c3d2824bba0de9052beaa2fd
ankitmehrotrachemistry <ankitmehrotrachemistry@gmail.com>
2022-08-05 18:57:50 +0530

    LINQ

---
+++ 7.3). LINQ_Standard_Query_Operators/LINQ_DescendingOrder/LINQ_DescendingOrder/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_DescendingOrder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 1, -2, 3, 0, -4, 5 };
+
+            var posNums = from n in nums
+                          where n > -5 && n < 6
+                          orderby n descending
+                          select n;
+
+            foreach (int i in posNums)
+                Console.Write(i + " ");
+
+            Console.WriteLine();
+        }
+    }
+}
