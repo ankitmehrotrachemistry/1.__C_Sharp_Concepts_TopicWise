820b39d82d143bc7d2907736b19bab2af6d4584e
ankitmehrotrachemistry <ankitmehrotrachemistry@gmail.com>
2022-08-26 14:42:02 +0530

    Message

---
+++ 6). Generic_Class_Template_Class_T/1). List, ArrayList/IList_Demo/IList_Demo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IList_Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IList<int> marks = new List<int>();
+
+            marks.Add(10);
+            marks.Add(20);
+            marks.Add(30);
+            marks.Add(40);
+
+            Console.Write("IList Elements are :");
+            foreach (var a in marks)
+                Console.Write(a + " ");
+            marks.RemoveAt(2);
+
+            Console.Write("\n");
+            Console.Write("IList after removal are : ");
+            foreach (var m in marks)
+                Console.Write(m + " ");
+
+            Console.Write("\n");
+        }
+    }
+}
