e963b92c8cdcbb73cb81be6da2fbde6a45f94d34
ankitmehrotrachemistry <ankitmehrotrachemistry@gmail.com>
2022-09-19 16:33:08 +0530

    Message

---
+++ 1.5). Functions(Methods)/Number_Palindrome_Function/Number_Palindrome_Function/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Number_Palindrome_Function
+{
+    class Program
+    {
+        static bool checkPalindrome(int n)
+        {
+            int reverse = 0;
+            int temp = n;
+
+            while (temp != 0) {
+                reverse = (reverse * 10) + (temp % 10);
+                temp = temp / 10;
+            }
+            return (reverse == n);    
+        }
+        static void Main(string[] args)
+        {
+            int num = 1234321;
+
+            if (checkPalindrome(num) == true)
+            {
+                Console.WriteLine("Yes, It is Palindrome!!");
+            }
+            else
+            {
+                Console.WriteLine("Not a Palindrome!!");
+            }
+        }
+    }
+}
