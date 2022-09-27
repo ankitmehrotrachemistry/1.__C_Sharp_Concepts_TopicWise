de308b1a7c36dfa3c1a08ec957415fec00340cfd
ankitmehrotrachemistry <ankitmehrotrachemistry@gmail.com>
2022-09-27 21:59:14 +0530

    Message

---
+++ 9.4). Predict the Output/LINQ_from_where_select/LINQ_from_where_select/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_from_where_select
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = {1,-2,3,0,-4,5};
+
+            var posNums = from n in nums
+                          where n >= 0
+                          select n;
+
+            foreach (int i in posNums)
+                Console.Write(i + " ");
+                Console.WriteLine();
+                Console.ReadLine();
+        }
+    }
+}
